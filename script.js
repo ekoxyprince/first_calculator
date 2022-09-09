a80e020c156fecf68e85b8e9594c26f072f7b92a
ekoxyprince <64290248+ekoxyprince@users.noreply.github.com>
2022-09-09 01:08:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var outputScreen = document.getElementById("output");
+
+function display(num) {
+    outputScreen.value += num;
+}
+
+function calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value)
+    } catch (err) {
+        alert("invalid function")
+    }
+}
+
+function Clear() {
+    outputScreen.value = " ";
+}
+
+function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
